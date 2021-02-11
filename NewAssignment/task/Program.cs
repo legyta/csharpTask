e626c059adcaf7f13d26dd1a547603eaec5f0e30
Ligita <legyta@gmail.com>
2021-02-11 08:56:31 +0100

    reading xml file

---
+++ NewAssignment/task/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+
+namespace task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalC = 0;
+
+            XmlDocument xmlDoc = new XmlDocument();
+            string XMLpath = Directory.GetCurrentDirectory() + @"\testData.xml";
+            xmlDoc.Load(XMLpath);
+
+            XmlNodeList CList = xmlDoc.GetElementsByTagName("Weight");
+            totalC = CList.Count;
+            for (int i = 0; i < CList.Count; i++)
+            {
+                Console.WriteLine(CList[i].InnerText.ToString());
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine(totalC.ToString() + "entries in the sheet");
+            Console.ReadLine();
+        }
+    }
+}
