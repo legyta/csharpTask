0567d87577a45cc6ba0714131f05ac7d76dab4a8
Ligita <legyta@gmail.com>
2021-02-15 17:09:12 +0100

    final commit

--- NewAssignment/task/Program.cs
+++ NewAssignment/task/Program.cs
@@ -1,34 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 using System.IO;
-
+using System.Globalization;
 namespace task
 {
     class Program
     {
+        /* Note: new class added, to handle incoming data from various sources
+         e.g. array of data coming in from new xml file/sql dbo 
+
+         static List<Department> allDepartments =
+             new List<Department> {
+                 new Department { name = "Dep1" },
+                 new Department { name = "Dep2" }
+             };
+         */
         static void Main(string[] args)
         {
-            int totalC = 0;
+            int parcelData = 0;
 
             XmlDocument xmlDoc = new XmlDocument();
             string XMLpath = Directory.GetCurrentDirectory() + @"\testData.xml";
             xmlDoc.Load(XMLpath);
 
-            XmlNodeList CList = xmlDoc.GetElementsByTagName("Weight");
-            totalC = CList.Count;
-            for (int i = 0; i < CList.Count; i++)
+            XmlNodeList parcelList = xmlDoc.GetElementsByTagName("Parcel");
+            parcelData = parcelList.Count;
+            for (int i = 0; i < parcelList.Count; i++)
             {
-                Console.WriteLine(CList[i].InnerText.ToString());
-                System.Threading.Thread.Sleep(1000);
-            }
 
-            Console.WriteLine(Environment.NewLine);
-            Console.WriteLine(totalC.ToString() + "entries in the sheet");
-            Console.ReadLine();
+                var weightData = parcelList[i].SelectSingleNode("Weight").InnerText;
+                var priceData = parcelList[i].SelectSingleNode("Value").InnerText;
+
+                double weight;
+                double.TryParse(weightData, NumberStyles.Any, CultureInfo.InvariantCulture, out weight);
+                double price;
+                double.TryParse(priceData, NumberStyles.Any, CultureInfo.InvariantCulture, out price);
+
+                if (price > 1000)
+                {
+                    Console.WriteLine("package needs to be first checked by Insurance department");
+                    /* Note: extra code can be added later, to check the weight before sending data to final source */
+                }
+                if (weight < 1)
+                {
+                    Console.WriteLine("Mail department");
+                }
+                else if (weight < 10)
+                {
+                    Console.WriteLine("Regular department");
+                }
+                else
+                {
+                    Console.WriteLine("Heavy department");
+                }
+            }
         }
     }
 }
